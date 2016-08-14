7991265435499f6de9097ee4cf3d5d4bd9dfe068
thanh-ly <thanh.ly.1992@gmail.com>
2016-08-14 00:49:01 -0500

    incremental commit, adding enemy and player to app.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +17,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed * dt;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -25,12 +29,64 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+
+    this.StartX = 0;
+    this.StartY = 375;
+
+    this.x = this.StartX;
+    this.y = this.StartY;
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+// Parameter: dt, a time delta between ticks
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+};
 
+// Draw the enemy on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(direction) {
+
+  // this.tempX = this.x;
+  // this.tempY = this.y;
+
+  // switch(direction) {
+  //   case "left":
+  //     this.tempX = this.tempX - this.dx;
+  //     break;
+  //   case "right":
+  //     this.tempX = this.tempX + this.dx;
+  //     break;
+  //   case "up":
+  //     this.tempY = this.tempY - this.dy;
+  //     break;
+  //   case "down":
+  //     this.tempY = this.tempY + this.dy;
+  //     break;
+  // }
+};
+
+//instantiates objects enemy and player
+var enemy1 = new Enemy(0, 55, 45);
+var enemy2 = new Enemy(0, 140, 175);
+var enemy3 = new Enemy(0, 225, 60);
+
+var allEnemies = [];
+allEnemies.push(enemy1);
+allEnemies.push(enemy2);
+allEnemies.push(enemy3);
+
+var player = new Player;
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
