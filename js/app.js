9d81e3259cdd8d8eb16daecc9e8bc0aa78761b4d
thanh-ly <thanh.ly.1992@gmail.com>
2016-08-14 13:41:08 -0500

    incremental commit; added movement to enemy and player instances

--- js/app.js
+++ js/app.js
@@ -1,10 +1,5 @@
 // Enemies our player must avoid
 var Enemy = function(x, y, speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
     this.x = x;
     this.y = y;
@@ -13,11 +8,19 @@ var Enemy = function(x, y, speed) {
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
+// Moves the enemy and executes reset
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    //@@@@confused as to how this works. esp the part where this.x += this.speed * dt  ~TL
     this.x += this.speed * dt;
+    this.reset();
+};
+
+// Reset enemy location upon reaching randomly generated endpoint range
+Enemy.prototype.reset = function() {
+    if (this.x >= randomInt(500, 800)) {
+        this.x = -110;
+        this.speed = randomInt(100, 400);
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -25,23 +28,16 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
+//defines Player class
 var Player = function() {
     this.sprite = 'images/char-boy.png';
-
-    this.StartX = 0;
-    this.StartY = 375;
-
+    //defines the start location
+    this.StartX = 202;
+    this.StartY = 390;
+    //locates player
     this.x = this.StartX;
     this.y = this.StartY;
 };
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
 
 // Parameter: dt, a time delta between ticks
 Player.prototype.update = function(dt) {
@@ -55,38 +51,50 @@ Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+
 Player.prototype.handleInput = function(direction) {
-
-  // this.tempX = this.x;
-  // this.tempY = this.y;
-
-  // switch(direction) {
-  //   case "left":
-  //     this.tempX = this.tempX - this.dx;
-  //     break;
-  //   case "right":
-  //     this.tempX = this.tempX + this.dx;
-  //     break;
-  //   case "up":
-  //     this.tempY = this.tempY - this.dy;
-  //     break;
-  //   case "down":
-  //     this.tempY = this.tempY + this.dy;
-  //     break;
-  // }
+    if (direction === 'left' && this.x !== borders.left) {
+        this.x -= 101;
+    }
+    if (direction === 'right' && this.x !== borders.right) {
+        this.x += 101;
+    }
+    if (direction === 'up' && this.y !== borders.top) {
+        this.y -= 85;
+    }
+    if (direction == 'down' && this.y !== borders.bottom) {
+        this.y += 85;
+    //resets the player position when top border is reached
+    } else if (direction === 'up' && this.y === 50) {
+        this.reset();
+    }
 };
 
-//instantiates objects enemy and player
-var enemy1 = new Enemy(0, 55, 45);
-var enemy2 = new Enemy(0, 140, 175);
-var enemy3 = new Enemy(0, 225, 60);
+// defines player borders
+var borders = {
+    left: 0,
+    right: 404,
+    bottom: 390,
+    top: 50
+};
+//instantiates enemy object, passing in x, y, speed
+var enemy1 = new Enemy(-110, 55, randomInt(100, 400));
+var enemy2 = new Enemy(-110, 140, randomInt(100, 400));
+var enemy3 = new Enemy(-110, 225, randomInt(100, 400));
 
+//pushes enemy ojects into allEnemies
 var allEnemies = [];
 allEnemies.push(enemy1);
 allEnemies.push(enemy2);
 allEnemies.push(enemy3);
 
+//instantiates player object
 var player = new Player;
+
+//random positive integer generator
+function randomInt (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
